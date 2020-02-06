1217b8ad39945d4650af4de74d98a0963850eab6
Your Name <matteoforni@hotmail.com>
2020-02-06 16:13:42 +0100

    Creato metodo home per admin page

--- codice/ValutazioneLPI/app/Http/Controllers/AdminController.php
+++ codice/ValutazioneLPI/app/Http/Controllers/AdminController.php
@@ -6,6 +6,19 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-
+/**
+     * Funzione che reindirizza l'utente alla pagina di registrazione.
+     */
+    public function home(Request $request){
+        //Verifico che l'utente sia un admin
+        if($request->all()['id_role'] == 2){
+            //Se è ammministratore gli mostro la pagina
+            return view('admin/index');
+        }else{
+            //Se non lo è ritorno l'errore
+            return response()->json(['Unauthorized' => 'Non hai i permessi necessari per accedere'], 401);
+        }
+       
+    }
 }
 ?>

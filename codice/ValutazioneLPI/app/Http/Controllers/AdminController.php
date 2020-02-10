8186571c190f6f5e97cfbb3096331ba874d65452
Your Name <matteoforni@hotmail.com>
2020-02-10 15:28:36 +0100

    creato metodi di eliminazione

--- codice/ValutazioneLPI/app/Http/Controllers/AdminController.php
+++ codice/ValutazioneLPI/app/Http/Controllers/AdminController.php
@@ -83,5 +83,58 @@ class AdminController extends Controller
             return response()->json(['Unauthorized' => 'Non hai i permessi necessari per accedere'], 401);
         }    
     }
+
+    /**
+     * Funzione che consente di eliminare un utente
+     * @param int id L'id dell'utente
+     * @param Request request La richiesta eseguita
+     * @return La risposta in JSON
+     */
+    public function deleteUser($id, Request $request){
+        //Verifico che l'utente sia un admin
+        if($request->all()['id_role'] == 2){
+            //Se è amministratore ritorno tutti gli utenti
+            User::findOrFail($id)->delete();
+            return response('Deleted Successfully', 200);
+        }else{
+            //Se non lo è ritorno l'errore
+            return response()->json(['Unauthorized' => 'Non hai i permessi necessari per accedere'], 401);
+        }    
+    }
+
+    public function updateUser($request, $id){
+        $this->validate($request, [
+            'name' => 'required|min:2|max:50',
+            'surname' => 'required',
+            'nationality' => 'required'
+        ]);
+        
+        $author = User::findOrFail($id);
+        $author->update($request->all());
+
+        return response()->json($author, 200);
+    }
+
+    /**
+     * Funzione che consente di eliminare una motivazione
+     * @param int id L'id della motivazione
+     * @param Request request La richiesta eseguita
+     * @return La risposta in JSON
+     */
+    public function deleteJustification($id, Request $request){
+        //Verifico che l'utente sia un admin
+        if($request->all()['id_role'] == 2){
+            //Se è amministratore ritorno tutti gli utenti
+            Justification::findOrFail($id)->delete();
+            return response('Deleted Successfully', 200);
+        }else{
+            //Se non lo è ritorno l'errore
+            return response()->json(['Unauthorized' => 'Non hai i permessi necessari per accedere'], 401);
+        }    
+    }
+
+    public function updateJustification($id){
+        
+    }
 }
 ?>

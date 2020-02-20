9e771f8e20e9eaedb7de4d82c0c52554cbfe94e8
Your Name <matteoforni@hotmail.com>
2020-02-20 16:17:11 +0100

    aggiunto tabella alla pagina dei docenti

--- codice/ValutazioneLPI/app/Http/Controllers/TeacherController.php
+++ codice/ValutazioneLPI/app/Http/Controllers/TeacherController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use App\Justification;
+use App\Form;
 use Illuminate\Http\Request;
 
 class TeacherController extends Controller
@@ -20,5 +21,22 @@ class TeacherController extends Controller
             //Se non lo è ritorno l'errore
             return response()->json(['Unauthorized' => 'Non hai i permessi necessari per accedere'], 401);
         }
+    }
+
+    /**
+     * Funzione che ritorna tutti gli utenti contenuti nel database
+     * @param Request request La richiesta eseguita
+     * @return La risposta in JSON
+     */
+    public function getForms(Request $request){
+        //Verifico che l'utente sia un admin
+        if($request->all()['user_id_role'] == 1){
+            //Se è amministratore ritorno tutti gli utenti
+            return response()->json(Form::all());
+        }else{
+            //Se non lo è ritorno l'errore
+            return response()->json(['Unauthorized' => 'Non hai i permessi necessari per accedere'], 401);
+        }
+        
     }
 }
--- codice/ValutazioneLPI/resources/views/teacher/index.blade.php
+++ codice/ValutazioneLPI/resources/views/teacher/index.blade.php
@@ -4,16 +4,74 @@
 
 @section('content')
 <div class="row">
-    <h1>TEACHER</h1>
-</div>
-<script>
-$(document).ready(function(){
-    //Notifico la mancanza di un email verificata
-    var user = <?php echo $user ?>
+    <div class="col-md-10 text-center offset-1">
+        <h3 class="h3 text-center my-5">Storico dei formulari</h3>
+        <div class="errors-forms text-danger">
 
-    if(user['confirmed'] == 0) {
-        toastr.warning('Verifica la tua email');
+        </div>
+        <div class="forms-table table-responsive mb-5">
+        </div>
+    </div>
+</div>
+
+<script src="/resources/js/JSONToHTML.js"></script>
+<script>
+    $(document).ready(function(){
+        //Notifico la mancanza di un email verificata
+        var user = <?php echo $user ?>
+
+        if(user['confirmed'] == 0) {
+            toastr.warning('Verifica la tua email');
+        }
+
+        createFormsTable();
+    });
+
+    /**
+     * Funzione che consente di creare la tabella dello storico
+     */ 
+     function createFormsTable(){
+        //Richiedo i dati per riempire la tabella
+        $.ajax({
+            type: "get",
+            url: "{{ url('teacher/forms') }}",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",     
+            headers: {
+                'Authorization':'Bearer ' + Cookies.get('token'),
+            },       
+            complete: function(response){
+                if(response["status"] == 200){
+                    var data = [];
+                    for(var item in response["responseJSON"]){
+                        var created = response["responseJSON"][item]['created'].split(" ")[0];
+                        var obj = {'title': response["responseJSON"][item]['title'], 'creato': created, 'Email allievo': response["responseJSON"][item]['student_email'], 'Email docente': response["responseJSON"][item]['teacher_email']};
+                        data.push(obj);
+                    }
+                    
+                    var table = JSONToHTML('Form', data);
+                    $(".forms-table").html(table);  
+                    $("#Form").DataTable({
+                        "searching": true,
+                        "ordering": false,
+                        "bLengthChange": false,
+                        "info" : false,
+                        "iDisplayLength": 5,
+                        "oLanguage": {
+                            "sEmptyTable": "Nessun formulario da mostrare",
+                            "sSearch": "Cerca formulari",
+                            "oPaginate": {
+                                "sFirst": "Prima pagina",
+                                "sPrevious": "Pagina precedente", 
+                                "sNext": "Prossima pagina", 
+                                "sLast": "Ultima pagina"
+                            }
+                        }
+                    });
+                }       
+            }
+        });
     }
-});
+
 </script>
 @endsection

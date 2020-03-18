7fa9c00d1572049ca44f4c16c68061f0b2c529bf
Your Name <matteoforni@hotmail.com>
2020-03-18 15:50:19 +0100

    terminato creazione PDF

--- codice/ValutazioneLPI/app/Http/Controllers/PDFController.php
+++ codice/ValutazioneLPI/app/Http/Controllers/PDFController.php
@@ -37,26 +37,46 @@ class PDFController extends TCPDF{
         $this->Cell(215, 10, 'Versione 2.0 (310119) - Ordinanza 2014', 0, false, 'C', 0, '', 0, false, 'T', 'M');
     }
 
+    /**
+     * Funzione che ritorna il formulario con tutti i suoi dati
+     * @return Il formulario in uso
+     */
     public function getForm(){
         $parts = explode("/", $_SERVER['REQUEST_URI']);
         $this->form = Form::find(end($parts));
     }
 
+    /**
+     * Funzione che ritorna lo studente collegato al formulario
+     * @return I dati dello studente
+     */
     public function getStudent(){
         $student = (object)["name" => $this->form['student_name'] . " " . $this->form['student_surname'], "phone" => $this->form['student_phone'], "email" => $this->form['student_email']];
         return $student;
     }
 
+    /**
+     * Funzione che ritorna il docente collegato al formulario
+     * @return I dati del docente
+     */
     public function getTeacher(){
         $teacher = (object)["name" => $this->form['teacher_name'] . " " . $this->form['teacher_surname'], "phone" => $this->form['teacher_phone'], "email" => $this->form['teacher_email']];
         return $teacher;
     }
     
+    /**
+     * Funzione che ritorna il primo perito collegato al formulario
+     * @return I dati del primo perito
+     */
     public function getExpert1(){
         $expert1 = (object)["name" => $this->form['expert1_name'] . " " . $this->form['expert1_surname'], "phone" => $this->form['expert1_phone'], "email" => $this->form['expert1_email']];
         return $expert1;
     }
 
+    /**
+     * Funzione che ritorna il secondo perito collegato al formulario
+     * @return I dati del secondo perito
+     */
     public function getExpert2(){
         $expert2 = (object)["name" => $this->form['expert2_name'] . " " . $this->form['expert2_surname'], "phone" => $this->form['expert2_phone'], "email" => $this->form['expert2_email']];
         return $expert2;
@@ -64,29 +84,36 @@ class PDFController extends TCPDF{
 
     /**
      * Funzione che ritorna tutte le motivazioni collegate ad un formulario
-     * @param Request request La richiesta eseguita 
-     * @param id L'id del formulario
-     * @return La risposta in JSON
+     * @return L'array con le motivazioni
      */
     public function getFormJustifications(){
+        //Ottengo tutte le motivazioni del formulario
         $contains = Contains::where('id_form', $this->form['id'])->get();
+        //Ottengo i punti specifici del formulario
         $points = Has::where('id_form', $this->form['id'])->get();
+
+        //Creo un array che ha come chiavi il codice del punto con una A davanti
         $justificationPoints = array();
         foreach($points as $point){
             $key = "A".$point->id_point;
             $justificationPoints[$key] = "";
         }
+        //Creo l'array che andrà a contenere le motivazioni della sezione A
         $justificationA = array("A1" => "", "A2" => "", "A3" => "", "A4" => "", "A5" => "", "A6" => "", "A7" => "", "A8" => "", "A9" => "", "A10" => "", "A11" => "", "A12" => "", "A13" => "");
+        //Unisco i punti specifici così da avere un'array con tutte le righe della tabella della sezione A
         $justificationA = array_merge($justificationA, $justificationPoints);
         
-        
+        //Creo l'array che andrà a contenere le motivazioni della sezione B
         $justificationB = array("B1" => "", "B2" => "", "B3" => "", "B4" => "", "B5" => "", "B6" => "", "B7" => "", "B8" => "", "B9" => "", "B10" => "");
+        //Creo l'array che andrà a contenere le motivazioni della sezione C
         $justificationC = array("C1" => "", "C2" => "", "C3" => "", "C4" => "", "C5" => "", "C6" => "", "C7" => "", "C8" => "", "C9" => "", "C10" => "");
 
-        $count = 11;
+        //Per ogni collegamento tra formulario e motivazione
         foreach($contains as $item){
+            //Carico i dati della motivazione
             $justification = Justification::find($item->id_justification);
              
+            //Verifico se va nella sezione A, B o C e la aggiungo alla rispettiva sezione
             if(substr($justification->id_point, 0, 1) === "A"){
                 $justificationA[$justification->id_point] .= $justification->text . "<br>";
             }else if(substr($justification->id_point, 0, 1) === "B"){
@@ -95,9 +122,10 @@ class PDFController extends TCPDF{
                 $justificationC[$justification->id_point] .= $justification->text . "<br>";
             }else{
                 $justificationA["A".$justification->id_point] .= $justification->text . "<br>";
-                $count++;
             }
         }
+
+        //Cambio le chiavi dei punti specifici con quelle della tabella (da A14 a A20)
         $mom = 14;
         foreach($justificationPoints as $key => $item){
             $justificationA["A".$mom] = strval($justificationA[$key]);
@@ -105,70 +133,93 @@ class PDFController extends TCPDF{
             $mom++;
         }
         
+        //Unisco le tre sezioni in un array
         $justifications = array();
-        if(count($justificationA) > 0){
-            $justifications['A'] = $justificationA;
-        }
-        if(count($justificationB) > 0){
-            $justifications['B'] = $justificationB;
-        }
-        if(count($justificationC) > 0){
-            $justifications['C'] = $justificationC;
-        }
+        
+        $justifications['A'] = $justificationA;
+        $justifications['B'] = $justificationB;
+        $justifications['C'] = $justificationC;
 
-        //Ritorno la risposta di successo.
+        //Ritorno la risposta
         return $justifications;
     }
 
-    public function getResults(){
+    /**
+     * Funzione che ritorna i punti per le sezioni
+     */
+    public function getPoints(){
+        //Carico i punti della prima sezione
         $query = "select * from contains inner join justification on contains.id_justification = justification.id where justification.id_point like 'A%' and contains.id_form = " . $this->form['id'];
-        $resultA = DB::select($query);
-        $query = "select * from contains inner join justification on contains.id_justification = justification.id where justification.id_point like 'B%' and contains.id_form = " . $this->form['id'];
-        $resultB = DB::select($query);
-        $query = "select * from contains inner join justification on contains.id_justification = justification.id where justification.id_point like 'C%' and contains.id_form = " . $this->form['id'];
-        $resultC = DB::select($query);
+        $pointsAmom = count(DB::select($query));
         $query = "select * from contains inner join justification on contains.id_justification = justification.id where justification.id_point NOT REGEXP '[a-zA-Z]' and contains.id_form = " . $this->form['id'];
-        $resultPoints = DB::select($query);
-        $result = array("A" => $resultA, "B" => $resultB, "C" => $resultC, "points" => $resultPoints);
-        return $result;
+        $pointsA = 60-($pointsAmom+count(DB::select($query)));
+
+        //Carico i punti della seconda sezione
+        $query = "select * from contains inner join justification on contains.id_justification = justification.id where justification.id_point like 'B%' and contains.id_form = " . $this->form['id'];
+        $pointsB = 30-count(DB::select($query));
+
+        //Carico i punti della terza sezione
+        $query = "select * from contains inner join justification on contains.id_justification = justification.id where justification.id_point like 'C%' and contains.id_form = " . $this->form['id'];
+        $pointsC = 30-count(DB::select($query));
+        
+        //ritorno i punteggi in un array unico
+        $points = array("A" => $pointsA, "B" => $pointsB, "C" => $pointsC);
+        return $points;
     }
 }
 
-// create new PDF document
+//Creo il PDF
 $pdf = new PDFController(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+//Imposto i margini
 $pdf->SetMargins("20", "15", "20");
 
+//Ottengo il formulario
 $pdf->getForm();
 
+//Ottengo l'allievo
 $student = $pdf->getStudent();
 
+//Ottengo il docente
 $teacher = $pdf->getTeacher();
 
+//Ottengo il primo perito
 $expert1 = $pdf->getExpert1();
 
-$expert2 = $pdf->getExpert1();
+//Ottegno il secondo perito
+$expert2 = $pdf->getExpert2();
 
-$result = $pdf->getResults();
-$resultPoints = $result['points'];
-$pointsA = 60-count($result['A'])+count($resultPoints);
+//Ottengo i punti
+$points = $pdf->getPoints();
+
+//Salvo i punti della prima sezione e ne calcolo la nota
+$pointsA = $points['A'];
 $resultA = round($pointsA/60*5+1,1);
-$pointsB = 30-count($result['B']);
+
+//Salvo i punti della seconda sezione e ne calcolo la nota
+$pointsB = $points['B'];
 $resultB = round($pointsB/30*5+1,1);
-$pointsC = 30-count($result['C']);
+
+//Salvo i punti della terza sezione e ne calcolo la nota
+$pointsC = $points['C'];
 $resultC = round($pointsC/30*5+1,1);
+
+//Calcolo la nota finale
 $finalResult = round($resultA*0.5 + $resultB *0.25 + $resultC * 0.25,1);
 
+//Carico tutte le motivazioni
 $justifications = $pdf->getFormJustifications();
 
+//Divido le sezioni e le salvo come oggetti
 $justificationA = (object)$justifications['A'];
 $justificationB = (object)$justifications['B'];
 $justificationC = (object)$justifications['C'];
 
 
-// add a page
+//Aggiungo una pagina
 $pdf->AddPage('P', 'A4');
 $pdf->SetY(10);
 
+//Genero la prima pagina
 $content = <<<EOF
 <style>
 h1 {
@@ -265,10 +316,13 @@ Il superiore professionale e gli esperti valutano le competenze professionali al
 
 Unicamente in caso di disaccordo tra periti e superiore professionale nella valutazione indicare i punti assegnati dai periti in Pt.Pe ed utilizzare il campo note (punto 7). </p>
 EOF;
+//Scrivo nel file la pagina
 $pdf->writeHTML($content);
 
+//Aggiungo una pagina
 $pdf->AddPage();
 
+//Genero la seconda pagina
 $content = <<<EOF
 <style>
 h1 {
@@ -371,11 +425,13 @@ td {
     </tr>
 </table>
 EOF;
-
+//Scrivo nel file la pagina
 $pdf->writeHTML($content);
 
+//Aggiungo una pagina
 $pdf->AddPage();
 
+//Genero la terza pagina
 $content = <<<EOF
 <style>
 h1 {
@@ -484,11 +540,13 @@ td {
 <br>
 <p>Si prega di utilizzare il catalogo criteri per completare questo modulo. Troverai i livelli di qualità per ogni domanda. Si possono assegnare unicamente punti interi (compresi tra 0 e 3). Si prega di motivare le valutazioni inferiori a 3.</p>
 EOF;
-
+//Scrivo nel file la pagina
 $pdf->writeHTML($content);
 
+//Aggiungo una pagina
 $pdf->AddPage();
 
+//Genero la quarta pagina
 $content = <<<EOF
 <style>
 h1 {
@@ -523,70 +581,70 @@ td {
         <td width="35"><b>B1</b></td>
         <td width="145">Riassunto della documentazione</td>
         <td width="30"></td>
-        <td width="240"></td>
+        <td width="240">$justificationB->B1</td>
         <td width="30"></td>    
     </tr>
     <tr>
         <td width="35"><b>B2</b></td>
         <td width="145">Tenuta del diario di lavoro</td>
         <td width="30"></td>
-        <td width="240"></td>
+        <td width="240">$justificationB->B2</td>
         <td width="30"></td>    
     </tr>
     <tr>
         <td><b>B3</b></td>
         <td>Capacità di riflessione</td>
         <td></td>
-        <td></td>
+        <td>$justificationB->B3</td>
         <td></td>    
     </tr>
     <tr>
         <td><b>B4</b></td>
         <td>Struttura</td>
         <td></td>
-        <td></td>
+        <td>$justificationB->B4</td>
         <td></td>    
     </tr>
     <tr>
         <td><b>B5</b></td>
         <td>Concisione</td>
         <td></td>
-        <td></td>
+        <td>$justificationB->B5</td>
         <td></td>    
     </tr>
     <tr>
         <td><b>B6</b></td>
         <td>Completezza formale della documentazione</td>
         <td></td>
-        <td></td>
+        <td>$justificationB->B6</td>
         <td></td>    
     </tr>
     <tr>
         <td><b>B7</b></td>
         <td>Espressione scritta e stile / ortografia e grammatica</td>
         <td></td>
-        <td></td>
+        <td>$justificationB->B7</td>
         <td></td>    
     </tr>
     <tr>
         <td><b>B8</b></td>
         <td>Rappresentazione</td>
         <td></td>
-        <td></td>
+        <td>$justificationB->B8</td>
         <td></td>    
     </tr>
     <tr>
         <td><b>B9</b></td>
         <td>Grafica, immagini, diagrammi e tabelle</td>
         <td></td>
-        <td></td>
+        <td>$justificationB->B9</td>
         <td></td>    
     </tr>
     <tr>
         <td><b>B10</b></td>
         <td>Documentazione della procedura di test e dei rispettivi risultati</td>
         <td></td>
-        <td></td>
+        <td>$justificationB->B10</td>
         <td></td>    
     </tr>
     <tr>
@@ -599,11 +657,13 @@ td {
 <br>
 <p>Si prega di utilizzare il catalogo criteri per completare questo modulo. Troverai i livelli di qualità per ogni domanda. Si possono assegnare unicamente punti interi (compresi tra 0 e 3). Si prega di motivare le valutazioni inferiori a 3.</p>
 EOF;
-
+//Scrivo nel file la pagina
 $pdf->writeHTML($content);
 
+//Aggiungo una pagina
 $pdf->AddPage();
 
+//Genero la quinta pagina
 $content = <<<EOF
 <style>
 h1 {
@@ -638,70 +698,70 @@ td {
         <td width="35"><b>C1</b></td>
         <td width="145">Gestione del tempo, struttura</td>
         <td width="30"></td>
-        <td width="240"></td>
+        <td width="240">$justificationC->C1</td>
         <td width="30"></td>    
     </tr>
     <tr>
         <td width="35"><b>C2</b></td>
         <td width="145">Utilizzo dei media, aspetti tecnici</td>
         <td width="30"></td>
-        <td width="240"></td>
+        <td width="240">$justificationC->C2</td>
         <td width="30"></td>    
     </tr>
     <tr>
         <td><b>C3</b></td>
         <td>Tecnica di presentazione</td>
         <td></td>
-        <td></td>
+        <td>$justificationC->C3</td>
         <td></td>    
     </tr>
     <tr>
         <td><b>C4</b></td>
         <td>Dimostrazione / presentazione del prodotto realizzato</td>
         <td></td>
-        <td></td>
+        <td>$justificationC->C4</td>
         <td></td>    
     </tr>
     <tr>
         <td><b>C5</b></td>
         <td>Domanda esperti 1</td>
         <td></td>
-        <td></td>
+        <td>$justificationC->C5</td>
         <td></td>    
     </tr>
     <tr>
         <td><b>C6</b></td>
         <td>Domanda esperti 2</td>
         <td></td>
-        <td></td>
+        <td>$justificationC->C6</td>
         <td></td>    
     </tr>
     <tr>
         <td><b>C7</b></td>
         <td>Domanda esperti 3</td>
         <td></td>
-        <td></td>
+        <td>$justificationC->C7</td>
         <td></td>    
     </tr>
     <tr>
         <td><b>C8</b></td>
         <td>Domanda esperti 4</td>
         <td></td>
-        <td></td>
+        <td>$justificationC->C8</td>
         <td></td>    
     </tr>
     <tr>
         <td><b>C9</b></td>
         <td>Domanda esperti 5</td>
         <td></td>
-        <td></td>
+        <td>$justificationC->C9</td>
         <td></td>    
     </tr>
     <tr>
         <td><b>C10</b></td>
         <td>Domanda esperti 6</td>
         <td></td>
-        <td></td>
+        <td>$justificationC->C10</td>
         <td></td>    
     </tr>
     <tr>
@@ -714,11 +774,13 @@ td {
 <br>
 <p>Si prega di utilizzare il catalogo criteri per completare questo modulo. Troverai i livelli di qualità per ogni domanda. Si possono assegnare unicamente punti interi (compresi tra 0 e 3). Si prega di motivare le valutazioni inferiori a 3.</p>
 EOF;
-
+//Scrivo nel file la pagina
 $pdf->writeHTML($content);
 
+//Aggiungo una pagina
 $pdf->AddPage();
 
+//Genero la sesta pagina
 $content = <<<EOF
 <style>
 h1 {
@@ -751,7 +813,7 @@ td {
     <tr>
         <td><b>A. Competenze professionali</b></td>
         <td><b>60</b></td>
-        <td>$pointsC</td>
+        <td>$pointsA</td>
         <td></td>
         <td>$resultA</td>
         <td></td>
@@ -889,12 +951,14 @@ Calcolo della nota finale = Nota A x 0.5 + Nota B x 0.25 + Nota C x 0.25 (nota a
     </tr>
 </table>
 EOF;
-
+//Scrivo nel file l'ultima pagina
 $pdf->writeHTML($content, true, 0, true, 0);
+//Chiudo il file
 $pdf->lastPage();
 
-//Close and output PDF document
+//Notifico al browser che invio un PDF
 header("Content-Type: application/pdf");
+//Stampo il PDF e chiudo l'output
 $pdf->Output();
 exit();
 ?>
